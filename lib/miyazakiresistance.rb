f8800fa94e238b3cfadfab8d6caeff6954d0a4be
tsukasa <tsukasa@ubuntu.(none)>
2009-03-21 21:09:26 +0900

    値変換メソッドの呼び出し不具合の修正

--- lib/miyazakiresistance.rb
+++ lib/miyazakiresistance.rb
@@ -228,7 +228,7 @@ module MiyazakiResistance
       hash = {}
       self.class.all_columns.each do |col, type|
         value = self.__send__(col)
-        value = convert_date_to_i(value, type)
+        value = self.convert_date_to_i(value, type)
         hash.update(col.to_s => value)
       end
       hash
