d325c8a04264ab6667a7cefeffbe6415e4b0962f
tsukasa <tsukasa@ubuntu.(none)>
2009-03-28 08:06:28 +0900

    バージョンアップ

--- lib/miyazakiresistance.rb
+++ lib/miyazakiresistance.rb
@@ -13,7 +13,7 @@ require 'miyazaki_resistance/base'
 require 'miyazaki_resistance/error'
 
 module MiyazakiResistance
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 end
 
 MiyazakiResistance::Base.class_eval do
